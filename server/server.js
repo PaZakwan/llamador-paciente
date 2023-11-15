d2ad147b8d1b61fb61f8a820700826a90e83865f
Juan Paz <jpazvillar88@hotmail.com>
2023-11-15 13:37:02 -0300

    update

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
 const {networkInterfaces} = require("os");
 const express = require("express");
-const socketIO = require("socket.io");
-const http = require("http");
+const {Server} = require("socket.io");
+const {createServer} = require("http");
 
 const path = require("path");
 
 const app = express();
-let server = http.createServer(app);
+const httpServer = createServer(app);
 
 const port = process.env.PORT || 80;
 
 app.use(express.static(path.resolve(__dirname, "../public")));
 
 // IO = esta es la comunicacion del backend
-module.exports.io = socketIO(server);
+module.exports.io = new Server(httpServer, {
+  /* options */
+  allowEIO3: true, // false by default
+});
 require("./sockets/socket");
 
-server.listen(port, (err) => {
+httpServer.listen(port, (err) => {
   if (err) throw new Error(err);
 
   let ipServer = Object.values(networkInterfaces())
