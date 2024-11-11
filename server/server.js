ee2371784e682953e8f2536dc7351369abf5459f
Juan Paz <jpazvillar88@hotmail.com>
2024-11-11 15:47:55 -0300

    +mongoose

--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
 import express from "express";
-import {Server} from "socket.io";
 import {createServer} from "http";
+import {Server} from "socket.io";
 
-// ============================
 // Variable global para la ruta donde corre el server
-// ============================
 import {resolve} from "path";
 process.env.MAIN_FOLDER = resolve(import.meta.dirname);
 
+// Variables de Configuracion
 await import("#MAIN_FOLDER/config.js");
 
+// Creando y configurando WebServer
 const app = express();
 const httpServer = createServer(app);
 
 app.use(express.static(resolve(process.env.MAIN_FOLDER, "../public")));
 
-// IO = esta es la comunicacion del backend
+// MONGODB CONEXIONES DB
+const {startConnectionDB} = await import("#MAIN_FOLDER/db_connection.js");
+startConnectionDB();
+
+// SOCKET IO = esta es la comunicacion del backend (add Socket listen en el WebServer)
 export const io = new Server(httpServer, {
   /* options */
   connectionStateRecovery: {
@@ -24,16 +28,18 @@ export const io = new Server(httpServer, {
   },
   // serveClient: false, //dont send "/socket.io/socket.io.js" to front in GET
 });
+// Cargando Eventos
 import("#MAIN_FOLDER/sockets/socket.js");
 
+// Ejecutando WebServer
 httpServer.listen(process.env.PORT, (error) => {
   if (error) {
     console.error(`startServer CATCH => ${error.name}: ${error.message}.`);
   }
 
   console.log(
-    `${new Date().toISOString()} <=> Ejecutando Servidor en: ${JSON.parse(
+    `===== ${new Date().toISOString()} <=> Ejecutando Servidor en: ${JSON.parse(
       process.env.BASE_URL
-    ).join("; ")}.`
+    ).join("; ")}. =====`
   );
 });
