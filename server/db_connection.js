dbf278d77015c18c7a7f10483ddd656b46cf231d
Juan Paz <jpazvillar88@hotmail.com>
2024-11-11 15:47:35 -0300

    +mongoose

--- server/db_connection.js
+++ server/db_connection.js
@@ -1,9 +1,4 @@
-const mongoose = require("mongoose");
-
-// ############
-// DESARROLLAR / REVISAR
-// Conexion a MongoDB
-// ############
+import mongoose from "mongoose";
 
 if (process.env.NODE_ENV === "dev") {
   mongoose.set("debug", true);
@@ -12,19 +7,19 @@ mongoose.set("allowDiskUse", true);
 
 // config default connection
 mongoose.connection
-  .on("error", function (err) {
+  .on("error", (err) => {
     // Primer conexion falla
     mensajeReintentando("Default");
     // console.error(`mongoose.connect ERROR (Default), ${err.name}: ${err.message}.`);
     // let defaultConnection = this;
     // intenta reconectar pasados los 20 seg de un error
-    setTimeout(function () {
+    setTimeout(() => {
       // console.log("****** mongoose Default models", defaultConnection.models);
       mongoose.connect(process.env.URLDB, JSON.parse(process.env.DBoptions)).catch((err) => {});
       // }, 20 * 1000);
     }, 5 * 1000);
   })
-  .on("connected", function () {
+  .on("connected", () => {
     mensajeONLINE("Default");
   })
   .on("disconnected", () => {
@@ -32,20 +27,11 @@ mongoose.connection
   });
 
 // FUNCION PARA INICIAR CONEXION A LA BD MONGO
-const startConnectionDB = async () => {
+export const startConnectionDB = async () => {
   try {
     // BASE
-    mongoose.connect(process.env.URLDB, JSON.parse(process.env.DBoptions)).catch((err) => {});
+    await mongoose.connect(process.env.URLDB, JSON.parse(process.env.DBoptions));
 
-    // UPLOAD
-    mongoose.upload = await crearNuevaConexion(
-      process.env.URLDB,
-      {
-        ...JSON.parse(process.env.DBoptions),
-        ...{maxPoolSize: 1},
-      },
-      "Upload"
-    );
     return mongoose;
   } catch (error) {
     console.error(`startConnectionDB CATCH => ${error.name}: ${error.message}.`);
@@ -53,64 +39,20 @@ const startConnectionDB = async () => {
   }
 };
 
-// FUNCION PARA CREAR OTRAS CONEXIONES A LA BD MONGO
-const crearNuevaConexion = async function (dbURL, options, baseName) {
-  let db = await mongoose
-    .createConnection(dbURL, options)
-    .on("error", function (err) {
-      // Primer conexion falla
-      mensajeReintentando(`${baseName}`);
-      // console.error(`createConnection ERROR (${baseName}), ${err.name}: ${err.message}.`);
-      let mongooseConnection = this;
-      // intenta reconectar pasados los 20 seg de un error
-      setTimeout(function () {
-        // buscar e intentar reconectar con THIS
-        // console.log(`****** mongoose.${baseName} models`, mongooseConnection.models);
-        mongooseConnection.openUri(dbURL, options).catch(() => {});
-        // }, 20 * 1000);
-      }, 5 * 1000);
-    })
-    .on("connected", function () {
-      mensajeONLINE(`${baseName}`);
-    })
-    .on("disconnected", () => {
-      mensajeOFFLINE(`${baseName}`);
-    })
-    .asPromise()
-    .catch((err) => {
-      // console.error(`createConnection CATCH (${baseName}), ${err.name}: ${err.message}.`);
-    });
-
-  return db;
-};
-
 // funciones de msjs
-const mensajeReintentando = function (db) {
-  console.error(`~~~~~ RE-Intentando primer conexion... (${db}) ~~~~~`);
+const mensajeReintentando = (db) => {
+  console.error(`~~~~~ RE-Intentando primer conexion... (${db}). ~~~~~`);
   // console.log(`mongoose.connections *Reintentando ${db}*`, mongoose.connections.length);
 };
-const mensajeONLINE = function (db) {
+const mensajeONLINE = (db) => {
   // if (db === "Default") {
   //   console.log(`===== Servidor funcionando en: ${process.env.BASE_URL}:${process.env.PORT} =====`);
   // }
-  console.log(`===== ${timeNow()} <=> Base de Datos ONLINE (${db}) =====`);
+  console.log(`===== ${new Date().toISOString()} <=> Base de Datos ONLINE (${db}). =====`);
   // console.log(`mongoose.connections *ONLINE ${db}*`, mongoose.connections.length);
 };
-const mensajeOFFLINE = function (db) {
-  console.error(`XXXXX ${timeNow()} <=> Base de Datos OFFLINE (${db}) XXXXX`);
+const mensajeOFFLINE = (db) => {
+  console.error(`XXXXX ${new Date().toISOString()} <=> Base de Datos OFFLINE (${db}). XXXXX`);
   console.error("XXXXX Esperando Re-conectar... XXXXX");
   // console.log(`mongoose.connections *OFFLINE ${db}*`, mongoose.connections.length);
 };
-const timeNow = function () {
-  let ahora = new Date();
-  return `${ahora.getFullYear()}/${(ahora.getMonth() + 1).toString().padStart(2, 0)}/${ahora
-    .getDate()
-    .toString()
-    .padStart(2, 0)} - ${ahora.getHours().toString().padStart(2, 0)}:${ahora
-    .getMinutes()
-    .toString()
-    .padStart(2, 0)}`;
-};
-
-// exports
-exports.startConnectionDB = startConnectionDB;
---
+++ server/models/llamador.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const schemaOptions = {
+  toObject: {
+    getters: true,
+  },
+  toJSON: {
+    getters: true,
+  },
+};
+
+const LlamadorSchema = new mongoose.Schema(
+  {
+    fecha: {
+      type: Date,
+      default: Date.now,
+    },
+    area: {
+      type: String,
+      trim: true,
+      required: [true, "El Area a gestionar es necesario."],
+    },
+
+    ultimosLlamados: {
+      type: [
+        {
+          nombre: {
+            type: String,
+            trim: true,
+          },
+          box: {
+            type: String,
+            trim: true,
+          },
+        },
+      ],
+      _id: false,
+      default: [],
+    },
+    personasEsperan: {
+      type: [
+        {
+          nombre: {
+            type: String,
+            trim: true,
+          },
+        },
+      ],
+      _id: false,
+      default: [],
+    },
+  },
+  schemaOptions
+);
+
+// LlamadorSchema.virtual("nombreC").get(function () {
+//   try {
+//     return `${this.nombre}${this.descripcion ? `: ${this.descripcion}` : ""}`;
+//   } catch (error) {
+//     return "ERROR nombre y descripcion";
+//   }
+// });
+
+export const Llamador = mongoose.model("Llamador", LlamadorSchema, "Llamadores");
